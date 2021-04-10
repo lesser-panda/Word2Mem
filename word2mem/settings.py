324ca16344c8761ebf0e5762cbc2df2639f9e8c7
Zhongyuan Zhang <me.zzy@me.com>
2021-04-09 22:30:44 -0500

    Changed default cache backend to Database caching (see settings.py). This prevents memory based cache related issues with pythonanywhere.com
    Also modified the .gitignore file to exclude PyCharm files.

--- word2mem/settings.py
+++ word2mem/settings.py
@@ -134,3 +134,10 @@ MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
 
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+        'LOCATION': 'my_cache_table',
+    }
+}
