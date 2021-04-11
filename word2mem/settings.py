ef276efeed29b886637215e7e50013e684303f95
Zhongyuan Zhang <me.zzy@me.com>
2021-04-10 21:57:10 -0500

    accidentally put the main static folder in the wrong place.

--- word2mem/settings.py
+++ word2mem/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'Word2Mem/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
